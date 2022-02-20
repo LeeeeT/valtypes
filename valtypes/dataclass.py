8aff52735b42b8fd5aef1fab66c0e83cbee46280
LeeeeT <leeeet@inbox.ru>
2022-02-20 20:29:23 +0300

    refactor: move `__all_annotations__` property to `Dataclass`'s metaclass

--- valtypes/dataclass.py
+++ valtypes/dataclass.py
@@ -1,26 +1,29 @@
-from __future__ import annotations
-
-from functools import reduce
-from operator import or_
+from typing import TYPE_CHECKING
 
 from .util import get_absolute_name
 
 __all__ = ["Dataclass"]
 
 
-class Dataclass:
+class DataclassMeta(type):
+    def __init__(cls, name: str, bases: tuple[type, ...], attrs: dict[str, object]):
+        super().__init__(name, bases, attrs)
+        cls.__all_annotations__: dict[str, object] = {}
+        cls._collect_annotations()
+
+    def _collect_annotations(cls) -> None:
+        all_annotations = [getattr(base, "__annotations__", {}) for base in cls.mro()]
+        for annotations in reversed(all_annotations):
+            cls.__all_annotations__.update(annotations)
+
+
+class Dataclass(metaclass=DataclassMeta):
+    if TYPE_CHECKING:
+        __all_annotations__: dict[str, object]
+
     def __init__(self, *args: object, **kwargs: object):
         for field, value in zip(tuple(kwargs.keys()) + tuple(self.__all_annotations__), tuple(kwargs.values()) + args):
             setattr(self, field, value)
-
-    @classmethod
-    @property
-    def __all_annotations__(cls) -> dict[str, object]:
-        return reduce(
-            or_,
-            (base.__dict__.get("__annotations__", {}) for base in reversed(cls.mro()) if issubclass(base, Dataclass)),
-            {},
-        )
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Dataclass):
@@ -38,4 +41,4 @@ class Dataclass:
 
     def __repr__(self) -> str:
         fields = (f"{field}={getattr(self, field)!r}" for field in self.__all_annotations__)
-        return f"{get_absolute_name(self.__class__)}({', '.join(fields)})"  # type: ignore
+        return f"{get_absolute_name(self.__class__)}({', '.join(fields)})"
--- valtypes/parsing/parser/dict_to_dataclass.py
+++ valtypes/parsing/parser/dict_to_dataclass.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, TypeVar, cast
+from typing import TYPE_CHECKING, Any, TypeVar
 
 from valtypes.dataclass import Dataclass
 from valtypes.parsing.error import ParsingError
@@ -19,7 +19,7 @@ T_Dataclass = TypeVar("T_Dataclass", bound=Dataclass)
 
 @convert
 def dict_to_dataclass(target_type: type[T_Dataclass], source: dict[Any, Any], collection: Collection) -> T_Dataclass:
-    if source.keys() < cast(dict[str, object], target_type.__all_annotations__).keys():
+    if source.keys() < target_type.__all_annotations__.keys():
         raise ParsingError(target_type, source)
     fields = {field: collection.parse(type, source[field]) for field, type in target_type.__all_annotations__.items()}
     return target_type(**fields)
