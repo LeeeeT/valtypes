1dc75b7d83eebc8bc37b71cbf2a1050f1a32559d
LeeeeT <leeeet@inbox.ru>
2022-05-14 13:48:31 +0300

    test: replace `pass_to_init` test with `error` test

--- tests/parsing/parser/test_to_constrained.py
+++ tests/parsing/parser/test_to_constrained.py
@@ -1,28 +1,11 @@
 from typing import Generic, TypeVar
 
-from valtypes import Constrained, condition, parse
+import pytest
+
+from valtypes import BaseParsingError, Constrained, condition, parse
+from valtypes.type.numeric import Portion
 
 T = TypeVar("T")
-
-
-def test_pass_to_init() -> None:
-    """
-    It triggers the constraint
-    """
-
-    constraint_triggered = False
-
-    def constraint(value: object, /) -> bool:
-        nonlocal constraint_triggered
-        constraint_triggered = True
-        return True
-
-    class MyConstrained(Constrained[object]):
-        __constraint__ = constraint
-
-    parse(MyConstrained, 1)
-
-    assert constraint_triggered
 
 
 def test_parse_to_bound() -> None:
@@ -46,3 +29,12 @@ def test_resolve_bound_type_args() -> None:
 
     assert parse(MyList[object], range(3)) == [0, 1, 2]
     assert parse(MyList[bytes], range(3)) == [b"0", b"1", b"2"]
+
+
+def test_error() -> None:
+    """
+    It raises an error if the value doesn't match the constraint
+    """
+
+    with pytest.raises(BaseParsingError):
+        parse(Portion, 2)
