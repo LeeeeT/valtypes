760fd828c7427649a52ede534b66245c2d0faf52
LeeeeT <leeeet@inbox.ru>
2022-05-14 15:17:30 +0300

    test: add tests for rules collection

---
+++ tests/parsing/test_collection.py
@@ -0,0 +1,24 @@
+from valtypes.condition import Is
+from valtypes.parsing import Collection, Rule
+from valtypes.parsing.parser import FromCallable
+
+
+def test_get_parsers_matching_type() -> None:
+    """
+    It collects all the parsers from the rules where the type matches the rule condition
+    """
+
+    collection = Collection([Rule(FromCallable(str), Is(str))])
+    assert collection.get_parsers_matching_type(str) == [FromCallable(str)]
+
+
+def test_clear_cache() -> None:
+    """
+    It clears the cache when adding a new rule
+    """
+
+    collection = Collection([Rule(FromCallable(str), Is(str))])
+    collection.get_parsers_matching_type(str)
+    collection.add(Rule(FromCallable(str), Is(str)))
+
+    assert collection.get_parsers_matching_type(str) == [FromCallable(str), FromCallable(str)]
