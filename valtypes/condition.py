cc8d5154a7bb36efde5ceabc0361dd9b6941b10f
LeeeeT <leeeet@inbox.ru>
2022-05-16 22:32:06 +0300

    feat: conditions are now dataclasses
    make conditions dataclasses to delete boilerplate code
    
    BREAKING CHANGE: `And` and `Or` conditions now takes a single tuple argument

--- valtypes/condition.py
+++ valtypes/condition.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import abc
 import re
 from collections.abc import Callable, Container
+from dataclasses import dataclass
 from typing import Any, Generic, TypeVar
 
 from .typing import GenericAlias, SupportsGe, SupportsGt, SupportsLe, SupportsLt, UnionType
@@ -45,16 +46,16 @@ class ABC(abc.ABC, Generic[T_contra]):
         pass
 
     def __and__(self, other: Callable[[Any], bool], /) -> And[T_contra]:
-        return And(self, other)
+        return And((self, other))
 
     def __rand__(self: ABC[Any], other: Callable[[T], bool], /) -> And[T]:
-        return And(other, self)
+        return And((other, self))
 
     def __or__(self: ABC[T], other: Callable[[T], bool], /) -> Or[T]:
-        return Or(self, other)
+        return Or((self, other))
 
     def __ror__(self: ABC[T], other: Callable[[T], bool], /) -> Or[T]:
-        return Or(other, self)
+        return Or((other, self))
 
     def __invert__(self) -> Not[T_contra]:
         return Not(self)
@@ -63,25 +64,25 @@ class ABC(abc.ABC, Generic[T_contra]):
         return Decorated(other, self)
 
 
+@dataclass
 class And(ABC[T], Generic[T]):
-    def __init__(self, *conditions: Callable[[T], bool]):
-        self.conditions = conditions
+    conditions: tuple[Callable[[T], bool], ...]
 
     def __call__(self, value: T, /) -> bool:
         return all(condition(value) for condition in self.conditions)
 
 
+@dataclass
 class Or(ABC[T], Generic[T]):
-    def __init__(self, *conditions: Callable[[T], bool]):
-        self.conditions = conditions
+    conditions: tuple[Callable[[T], bool], ...]
 
     def __call__(self, value: T, /) -> bool:
         return any(condition(value) for condition in self.conditions)
 
 
+@dataclass
 class Not(ABC[T], Generic[T]):
-    def __init__(self, condition: Callable[[T], bool], /):
-        self.condition = condition
+    condition: Callable[[T], bool]
 
     def __call__(self, value: T, /) -> bool:
         return not self.condition(value)
@@ -96,25 +97,25 @@ class Decorated(ABC[T], Generic[T]):
         return self.condition(self.decorator(value))
 
 
+@dataclass
 class Wrap(ABC[T], Generic[T]):
-    def __init__(self, condition: Callable[[T], bool], /):
-        self.condition = condition
+    condition: Callable[[T], bool]
 
     def __call__(self, value: T, /) -> bool:
         return self.condition(value)
 
 
+@dataclass
 class Is(ABC[object]):
-    def __init__(self, object: object, /):
-        self.object = object
+    object: object
 
     def __call__(self, value: object, /) -> bool:
         return value is self.object
 
 
+@dataclass
 class IsInstance(ABC[object]):
-    def __init__(self, type: type | UnionType, /):
-        self.type = type
+    type: type | UnionType
 
     def __call__(self, value: object, /) -> bool:
         try:
@@ -123,81 +124,81 @@ class IsInstance(ABC[object]):
             return False
 
 
+@dataclass
 class IsSubclass(ABC[object]):
-    def __init__(self, type: type | UnionType, /):
-        self.type = type
+    type: type | UnionType
 
     def __call__(self, value: object, /) -> bool:
         return isinstance(value, type) and issubclass(value, self.type)
 
 
+@dataclass
 class Equals(ABC[object]):
-    def __init__(self, obj: object, /):
-        self.object = obj
+    object: object
 
     def __call__(self, value: object, /) -> bool:
         return value == self.object
 
 
+@dataclass
 class LessThan(ABC[SupportsLt[T]], Generic[T]):
-    def __init__(self, exclusive_maximum: T, /):
-        self.exclusive_maximum = exclusive_maximum
+    exclusive_maximum: T
 
     def __call__(self, value: SupportsLt[T], /) -> bool:
         return value < self.exclusive_maximum
 
 
+@dataclass
 class LessEquals(ABC[SupportsLe[T]], Generic[T]):
-    def __init__(self, maximum: T, /):
-        self.maximum = maximum
+    maximum: T
 
     def __call__(self, value: SupportsLe[T], /) -> bool:
         return value <= self.maximum
 
 
+@dataclass
 class GreaterThan(ABC[SupportsGt[T]], Generic[T]):
-    def __init__(self, exclusive_minimum: T, /):
-        self.exclusive_minimum = exclusive_minimum
+    exclusive_minimum: T
 
     def __call__(self, value: SupportsGt[T], /) -> bool:
         return value > self.exclusive_minimum
 
 
+@dataclass
 class GreaterEquals(ABC[SupportsGe[T]], Generic[T]):
-    def __init__(self, minimum: T, /):
-        self.minimum = minimum
+    minimum: T
 
     def __call__(self, value: SupportsGe[T], /) -> bool:
         return value >= self.minimum
 
 
+@dataclass
 class Contains(ABC[Container[T]], Generic[T]):
-    def __init__(self, item: T, /):
-        self.item = item
+    item: T
 
     def __call__(self, value: Container[T], /) -> bool:
         return self.item in value
 
 
+@dataclass
 class In(ABC[T], Generic[T]):
-    def __init__(self, container: Container[T], /):
-        self.container = container
+    container: Container[T]
 
     def __call__(self, value: T, /) -> bool:
         return value in self.container
 
 
+@dataclass
 class Pattern(ABC[str]):
-    def __init__(self, pattern: re.Pattern[str], /):
-        self.pattern = pattern
+    pattern: re.Pattern[str]
 
     def __call__(self, value: str, /) -> bool:
         return bool(self.pattern.match(value))
 
 
+@dataclass
 class GenericAliasOf(ABC[object]):
-    def __init__(self, type: type, /):
-        self.type = type
+    type: type
 
     def __call__(self, value: object, /) -> bool:
         if not isinstance(value, GenericAlias):
@@ -208,9 +209,9 @@ class GenericAliasOf(ABC[object]):
             return False
 
 
+@dataclass
 class StrictGenericAliasOf(ABC[object]):
-    def __init__(self, type: type, /):
-        self.type = type
+    type: type
 
     def __call__(self, value: object, /) -> bool:
         return isinstance(value, GenericAlias) and value.__origin__ is self.type
